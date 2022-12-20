fb541d297023ba8ed5b3a6673d957d6aa85d434f
Sven Giebelmann <sven.giebelmann@adesso.de>
2022-12-20 15:31:30 +0100

    feat: add pokeapi types

---
+++ src/types/common.ts
@@ -0,0 +1,55 @@
+export interface Name {
+  name: string
+  language: NamedAPIResource
+}
+
+export interface NamedAPIResource {
+  name: string
+  url: string
+}
+
+export interface NamedAPIResourceList {
+  count: number
+  next: string | null
+  previous: string | null
+  results: NamedAPIResource[]
+}
+
+export interface APIResource {
+  url: string
+}
+
+export interface VersionEncounterDetail {
+  version: NamedAPIResource
+  max_chance: number
+  encounter_details: Encounter[]
+}
+
+export interface VersionGameIndex {
+  game_index: number
+  version: NamedAPIResource
+}
+
+export interface VersionGroupFlavorText {
+  text: string
+  language: NamedAPIResource
+  version_group: NamedAPIResource
+}
+
+export interface FlavorText {
+  flavor_text: string
+  language: NamedAPIResource
+}
+
+export interface Description {
+  description: string
+  language: NamedAPIResource
+}
+
+export interface Encounter {
+  min_level: number
+  max_level: number
+  condition_values: NamedAPIResource[]
+  chance: number
+  method: NamedAPIResource
+}
