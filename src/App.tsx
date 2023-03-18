3ae0e308acb60704d977fc836944f24e81f8b1a4
Sven Giebelmann <73178137+monktana@users.noreply.github.com>
2023-03-18 20:08:30 +0100

    ui: remove gameboy component

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import React from 'react'
 
 import './App.css'
 import { Game } from '@/features/quiz'
-import { AppProvider, LocalizationProvider } from '@/providers'
 
-import { GameBoy } from './features/gameboy/components/GameBoy'
 import { Menu } from './features/menu'
 import { useAppStateStore } from './stores'
 
@@ -12,15 +10,11 @@ function App(): JSX.Element | null {
   const appState = useAppStateStore((state) => state.appState);
 
   return (
-    <AppProvider>
-      <LocalizationProvider>
-        <GameBoy>
-          { appState === 'menu' && <Menu variant='mainmenu' />}
-          { appState === 'quiz' && <Game />}
-          { appState === 'gameover' && <Menu variant='gameover' />}
-        </GameBoy>
-      </LocalizationProvider>
-    </AppProvider>
+    <div className="content">
+      { appState === 'menu' && <Menu variant='mainmenu' />}
+      { appState === 'quiz' && <Game />}
+      { appState === 'gameover' && <Menu variant='gameover' />}
+    </div>
   )
 }
 
