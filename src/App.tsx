c6a81c29be05fca76d03f31dd3d5c0160b340cf4
Sven Giebelmann <sven.giebelmann@adesso.de>
2023-07-23 19:52:12 +0200

    ui: add wiggle animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import { Container } from '@chakra-ui/react';
+import { Container } from '@chakra-ui/react'
 
 import { Menu } from '@/features/menu'
 import { Game } from '@/features/quiz'
 
-import { Score } from './features/quiz/components/score';
+import { Score } from './features/quiz/components/score'
 import { useAppStateStore } from './stores'
 
 function App(): JSX.Element | null {
   const appState = useAppStateStore((state) => state.appState);
 
   return (
-    <Container minH="100vh" centerContent justifyContent="center">
+    <Container
+      h="100vh"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      backgroundColor="gray.300"
+    >
       <Score pos="absolute" top={2} left={4} />
       { appState === 'menu' && <Menu /> }
       { appState === 'quiz' && <Game /> }
--- src/features/menu/components/Menu/Menu.tsx
+++ src/features/menu/components/Menu/Menu.tsx
@@ -1,6 +1,19 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Button, keyframes } from '@chakra-ui/react';
+import { motion } from "framer-motion";
 
 import { useAppStateStore, useScoreStore } from '@/stores';
+
+const animationKeyframes = keyframes`
+  0% { transform: rotate(0) }
+  10% { transform: rotate(-25deg) }
+  30% { transform: rotate(17deg) }
+  60% { transform: rotate(-10deg) }
+  80% { transform: rotate(5deg) }
+  90% { transform: rotate(0) }
+  100% { transform: rotate(0) }
+`;
+
+const animation = `${animationKeyframes} 4s ease-in-out infinite`;
 
 export function Menu() {
   const startGame = useAppStateStore((state) => state.startQuiz);
@@ -8,6 +21,8 @@ export function Menu() {
 
   return (
     <Box
+      as={motion.div}
+      animation={animation}
       position="relative"
       w="xs"
       h="xs"
@@ -21,21 +36,19 @@ export function Menu() {
         backgroundColor="red.500"
         borderTopRadius="full"
       />
-      <Box
+      <Button
         position="absolute"
         top="50%"
         left="50%"
         transform={"translate(-50%, -50%)"}
         w="4rem"
         h="4rem"
-        backgroundColor="white"
+        bg="white"
         rounded="full"
         borderWidth="6px"
         borderColor="black"
         onClick={() => { resetScore(); startGame(); }}
       />
-      {/* <Flex alignItems="center" justifyContent="center">
-      </Flex> */}
     </Box>
   );
 }
