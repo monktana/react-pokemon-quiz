357870b3f506458b982df5b54e1c1fdf815adb00
monktana <sven@giebelmann.dev>
2023-08-26 22:21:56 +0200

    ui: orient score horizontally

--- src/features/quiz/components/Score/score.tsx
+++ src/features/quiz/components/Score/score.tsx
@@ -1,20 +1,20 @@
-import { Stat, StatGroup, StatGroupProps, StatLabel, StatNumber } from "@chakra-ui/react";
+import { HStack, Stat, StatProps, StatLabel, StatNumber } from "@chakra-ui/react";
 import { useContext } from "react";
 
 import { LocalizationContext } from "@/providers";
 import { useScoreStore } from "@/stores";
 
 
-export function Score(props: StatGroupProps) {
+export function Score(props: StatProps) {
   const score = useScoreStore((state) => state.score);
   const Localization = useContext(LocalizationContext);
   
   return (
-    <StatGroup {...props}>
-      <Stat>
+    <Stat {...props}>
+      <HStack>
         <StatLabel color="font.500">{Localization.getText("en", "score.label")}</StatLabel>
         <StatNumber color="font.100">{score}</StatNumber>
-      </Stat>
-    </StatGroup>
+      </HStack>
+    </Stat>
   )
 }
