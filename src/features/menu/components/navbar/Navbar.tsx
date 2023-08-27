d01a58bf3c65ddcf23d1d81db74af1ab3f4327c9
monktana <sven@giebelmann.dev>
2023-08-27 16:23:45 +0200

    ui: style language selection as Menu

--- src/features/menu/components/navbar/Navbar.tsx
+++ src/features/menu/components/navbar/Navbar.tsx
@@ -1,8 +1,10 @@
-import { Box, HStack, IconButton, Select } from "@chakra-ui/react";
+import { Box, HStack, IconButton, Menu, MenuButton, MenuItem, MenuList, Text } from "@chakra-ui/react";
 
 import { GhostIcon } from "@/features/quiz/components/icons";
 import { Languages } from "@/hooks/i18n";
 import { useLanguageStore } from "@/stores";
+
+import { LanguageIcon } from "../icons";
 
 export function Navbar() {
   const language = useLanguageStore((state) => state.language);
@@ -10,20 +12,19 @@ export function Navbar() {
 
   return (
     <Box position="fixed" zIndex="overlay" width="full" background="background.500">
-      <HStack align="center" justify="end" px={4}>
-        <Select
-          value={language}
-          color="white"
-          onChange={(event) => {
-            setLanguage(event.target.value)
-          }}
-        >
-          {Languages.map((language) => {
-            return (
-              <option key={language} value={language}>{language}</option>
-            )
-          })}
-        </Select>
+      <HStack align="center" justify="end" px={4} spacing={2}>
+        <Menu>
+          <MenuButton as={IconButton} icon={<LanguageIcon type={language} aria-label={language}/>} aria-label="change language" />
+          <MenuList>
+            {Languages.map((language) => {
+              return (
+                <MenuItem key={language} icon={<LanguageIcon type={language} />} onClick={() => setLanguage(language)}>
+                  <Text>{language}</Text>
+                </MenuItem>
+              )
+            })}
+          </MenuList>
+        </Menu>
         <IconButton aria-label="change color mode" icon={<GhostIcon/>} />
       </HStack>
     </Box>
