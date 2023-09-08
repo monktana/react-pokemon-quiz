178f764ee95edf62db1e3e0add848e86978bb1b3
monktana <sven@giebelmann.dev>
2023-09-08 21:22:01 +0200

    build: add sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import { ColorModeScript, theme } from '@chakra-ui/react';
+import * as Sentry from "@sentry/react";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { AppProvider, LocalizationProvider } from '@/providers';
 
 import App from './App';
+
+Sentry.init({
+  dsn: "https://a1b13ef884fb900bf80f6c81ba3090ce@o1160434.ingest.sentry.io/4505846438625280",
+  integrations: [
+    new Sentry.BrowserTracing({
+      tracePropagationTargets: ["localhost"],
+    }),
+    new Sentry.Replay(),
+  ],
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), sentryVitePlugin({
+    org: "sven-giebelmann",
+    project: "react-pokemon-quiz"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 });
