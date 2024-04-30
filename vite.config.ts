c9da00089e8cf7e66588414706c366afbca88e41
Sven Giebelmann <svengiebelmann@gmail.com>
2024-04-30 16:05:24 +0200

    build: generate sourceMaps only in dev, create dependency chunk

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,17 @@ import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig((configEnv) => ({
   plugins: [react(), tsconfigPaths()],
 
   build: {
-    sourcemap: true
-  }
-});
+    sourcemap: configEnv.mode === 'development',
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          return id.includes('node_modules') ? 'vendor' : null;
+        },
+      },
+    },
+  },
+}));
