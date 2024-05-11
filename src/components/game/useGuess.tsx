59ff4af03c71fba2c73d9aa325614651f2f956cb
Sven Giebelmann <svengiebelmann@gmail.com>
2024-05-11 10:55:57 +0200

    feat: end game on wrong guess

--- src/components/game/useGuess.tsx
+++ src/components/game/useGuess.tsx
@@ -2,21 +2,22 @@ import { UseSuspenseQueryResult } from '@tanstack/react-query';
 import { useCallback } from 'react';
 
 import { Matchup, TypeEffectiveness } from '@/api/schema';
-import { useScoreActions } from '@/stores';
+import { useAppStateActions, useScoreActions } from '@/stores';
 
 export const useGuess = (matchup: Matchup, refetch: UseSuspenseQueryResult['refetch']) => {
-  const { increase, decrease } = useScoreActions();
+  const { increase } = useScoreActions();
+  const { endQuiz } = useAppStateActions();
 
   const makeGuess = useCallback(
     (guess: TypeEffectiveness) => {
       if (matchup.effectiveness !== guess) {
-        decrease();
+        endQuiz();
         return;
       }
       increase();
       refetch();
     },
-    [decrease, increase, matchup.effectiveness, refetch]
+    [endQuiz, increase, matchup.effectiveness, refetch]
   );
 
   return { makeGuess };
