ce84c429fffa703254ee7806c08ca95df20e4aca
Sven Giebelmann <svengiebelmann@gmail.com>
2024-06-03 13:14:16 +0200

    tests: add coverage for score store

---
+++ src/stores/Score/ScoreStoreProvider.test.tsx
@@ -0,0 +1,136 @@
+import { act, renderHook } from '@testing-library/react';
+import { ReactNode } from 'react';
+import { describe, expect, it } from 'vitest';
+
+import { ScoreStoreProvider, ScoreStoreProviderProps, useScore, useScoreActions } from '@/stores';
+
+describe('LanguageStoreProvider', () => {
+  it('provides a hook to access the current score', () => {
+    const { result } = renderHook(() => useScore(), {
+      wrapper: createWrapper(ScoreStoreProvider, { initialScore: 1 }),
+    });
+
+    expect(result.current).toEqual(1);
+  });
+
+  it('provides a method to increase the score', () => {
+    const wrapper = createWrapper(ScoreStoreProvider, { initialScore: 0 });
+
+    const { result, rerender } = renderHook(
+      () => ({
+        score: useScore(),
+        actions: useScoreActions(),
+      }),
+      {
+        wrapper,
+      }
+    );
+
+    act(result.current.actions.increase);
+    rerender();
+
+    expect(result.current.score).toEqual(1);
+  });
+
+  it('provides a method to decrease the score', () => {
+    const wrapper = createWrapper(ScoreStoreProvider, { initialScore: 2 });
+
+    const { result, rerender } = renderHook(
+      () => ({
+        score: useScore(),
+        actions: useScoreActions(),
+      }),
+      {
+        wrapper,
+      }
+    );
+
+    act(result.current.actions.decrease);
+    rerender();
+
+    expect(result.current.score).toEqual(1);
+  });
+
+  it('provides a method to reset the score', () => {
+    const wrapper = createWrapper(ScoreStoreProvider, { initialScore: 5 });
+
+    const { result, rerender } = renderHook(
+      () => ({
+        score: useScore(),
+        actions: useScoreActions(),
+      }),
+      {
+        wrapper,
+      }
+    );
+
+    act(result.current.actions.reset);
+    rerender();
+
+    expect(result.current.score).toEqual(0);
+  });
+
+  it('resets a negative score', () => {
+    const wrapper = createWrapper(ScoreStoreProvider, { initialScore: -5 });
+
+    const { result, rerender } = renderHook(
+      () => ({
+        score: useScore(),
+        actions: useScoreActions(),
+      }),
+      {
+        wrapper,
+      }
+    );
+
+    act(result.current.actions.reset);
+    rerender();
+
+    expect(result.current.score).toEqual(0);
+  });
+
+  it('resets the score after multiple actions', () => {
+    const wrapper = createWrapper(ScoreStoreProvider, { initialScore: 5 });
+
+    const { result, rerender } = renderHook(
+      () => ({
+        score: useScore(),
+        actions: useScoreActions(),
+      }),
+      {
+        wrapper,
+      }
+    );
+
+    act(result.current.actions.increase);
+    act(result.current.actions.increase);
+    act(result.current.actions.decrease);
+    rerender();
+
+    expect(result.current.score).toEqual(6);
+
+    act(result.current.actions.reset);
+    rerender();
+
+    expect(result.current.score).toEqual(0);
+  });
+
+  it('causes the provided hooks to throw if provider is absent', () => {
+    renderHook(() => {
+      try {
+        useScore();
+      } catch (error) {
+        expect((error as Error).message).toEqual('Missing ScoreStoreProvider');
+      }
+    });
+  });
+});
+
+const createWrapper = (
+  Wrapper: (props: ScoreStoreProviderProps) => React.JSX.Element,
+  props: ScoreStoreProviderProps
+) => {
+  return function CreatedWrapper({ children }: { children: ReactNode }) {
+    return <Wrapper {...props}>{children}</Wrapper>;
+  };
+};
