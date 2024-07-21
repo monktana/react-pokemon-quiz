685fd302291741a6b276824f1ffc0910fd809f0f
Sven Giebelmann <svengiebelmann@gmail.com>
2024-07-21 15:33:22 +0200

    build: add playwright scripts

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,8 +14,6 @@ export default defineConfig({
   testDir: './tests',
   // Folder for test artifacts such as screenshots, videos, traces, etc.
   outputDir: './tests/results',
-  /* Run tests in files in parallel */
-  // fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -28,8 +26,11 @@ export default defineConfig({
   use: {
     // Base URL to use in actions like `await page.goto('/')`.
     baseURL: 'http://localhost:5173',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    // Emulates the user locale.
+    locale: 'en-GB',
+    // Take a screenshot when a test fails. See https://playwright.dev/docs/screenshots
+    screenshot: 'only-on-failure',
+    // Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer
     trace: 'on-first-retry',
   },
 
