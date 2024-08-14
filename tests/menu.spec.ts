dc022ab89ed2e7e9e3b06cb9caccb3d5f216da61
Sven Giebelmann <svengiebelmann@gmail.com>
2024-08-14 13:54:28 +0200

    tests: switch to getByTestId in menu tests

--- tests/menu.spec.ts
+++ tests/menu.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable testing-library/prefer-screen-queries */
 import { expect, test } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
@@ -8,9 +9,9 @@ test.beforeEach(async ({ page }) => {
 test.describe('Language Select', () => {
   test('enables the player to change the language', async ({ page }) => {
     // await expect(page).toHaveAttribute('html', 'lang', 'en');
-    await expect(page.locator('[data-testid="language-switch"]')).toBeVisible();
-    await expect(page.locator('[data-testid="language-switch"]')).toBeEnabled();
-    await expect(page.locator('[data-testid="language-switch"]')).toHaveAttribute(
+    await expect(page.getByTestId('language-switch')).toBeVisible();
+    await expect(page.getByTestId('language-switch')).toBeEnabled();
+    await expect(page.getByTestId('language-switch')).toHaveAttribute(
       'aria-label',
       'Change language'
     );
@@ -19,14 +20,14 @@ test.describe('Language Select', () => {
       'en'
     );
 
-    await page.locator('[data-testid="language-switch"]').click();
-    await expect(page.locator('[data-testid="en-language"]')).toBeVisible();
-    await expect(page.locator('[data-testid="de-language"]')).toBeVisible();
+    await page.getByTestId('language-switch').click();
+    await expect(page.getByTestId('en-language')).toBeVisible();
+    await expect(page.getByTestId('de-language')).toBeVisible();
 
-    await page.locator('[data-testid="de-language"]').click();
+    await page.getByTestId('de-language').click();
 
     // await expect(page).toHaveAttribute('html', 'lang', 'de');
-    await expect(page.locator('[data-testid="language-switch"]')).toHaveAttribute(
+    await expect(page.getByTestId('language-switch')).toHaveAttribute(
       'aria-label',
       'Sprache wechseln'
     );
@@ -44,14 +45,14 @@ test.describe('Color Mode', () => {
       // eslint-disable-next-line jest-dom/prefer-to-have-style
       await expect(page.locator('html')).toHaveAttribute('style', 'color-scheme: light;');
 
-      await expect(page.locator('[data-testid="color-mode-switch"]')).toBeVisible();
-      await expect(page.locator('[data-testid="color-mode-switch"]')).toBeEnabled();
-      await expect(page.locator('[data-testid="color-mode-switch"]')).toHaveAttribute(
+      await expect(page.getByTestId('color-mode-switch')).toBeVisible();
+      await expect(page.getByTestId('color-mode-switch')).toBeEnabled();
+      await expect(page.getByTestId('color-mode-switch')).toHaveAttribute(
         'aria-label',
         'Change color scheme'
       );
 
-      await page.locator('[data-testid="color-mode-switch"]').click();
+      await page.getByTestId('color-mode-switch').click();
 
       await expect(page.locator('html')).toHaveAttribute('data-theme', 'dark');
     });
@@ -64,7 +65,7 @@ test.describe('Color Mode', () => {
       // eslint-disable-next-line jest-dom/prefer-to-have-style
       await expect(page.locator('html')).toHaveAttribute('style', 'color-scheme: dark;');
 
-      await page.locator('[data-testid="color-mode-switch"]').click();
+      await page.getByTestId('color-mode-switch').click();
 
       await expect(page.locator('html')).toHaveAttribute('data-theme', 'light');
     });
