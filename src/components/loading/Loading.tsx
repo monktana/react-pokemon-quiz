5251cc603abb185547f3b025999c0cc2d56d8522
Sven Giebelmann <73178137+monktana@users.noreply.github.com>
2024-08-19 11:19:38 +0200

    feat: center content of Loading, update snapshot

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -1,32 +1,31 @@
 import React from 'react';
-import { Grid, Skeleton, VStack } from '@chakra-ui/react';
+import { Center, Grid, Skeleton, VStack } from '@chakra-ui/react';
 
 export const Loading = () => {
   return (
-    <VStack data-testid="loading-container" width="360px">
-      <Skeleton variant="quiz" height={12} width="full" />
-      <Skeleton variant="quiz" height="218px" width="full" />
-      <Skeleton variant="quiz" height="218px" width="full" />
-      <Skeleton variant="quiz" height={12} width="full" />
-      <Grid
-        gridTemplateColumns="repeat(2, 1fr)"
-        gap={2}
-        padding={2}
-        width="full"
-        rounded="md"
-        border="1px solid"
-        borderColor="border.500"
-        backgroundColor="background.200"
-        _dark={{
-          borderColor: 'border.100',
-          backgroundColor: 'background.800',
-        }}
-      >
-        <Skeleton variant="quiz" height={10} width="full" />
-        <Skeleton variant="quiz" height={10} width="full" />
-        <Skeleton variant="quiz" height={10} width="full" />
-        <Skeleton variant="quiz" height={10} width="full" />
-      </Grid>
-    </VStack>
+    <Center height="100vh">
+      <VStack data-testid="loading-container" width="360px">
+        <Skeleton variant="quiz" height={12} width="full" />
+        <Skeleton variant="quiz" height="218px" width="full" />
+        <Skeleton variant="quiz" height="218px" width="full" />
+        <Skeleton variant="quiz" height={12} width="full" />
+        <Grid
+          gridTemplateColumns="repeat(2, 1fr)"
+          gap={2}
+          padding={2}
+          width="full"
+          rounded="md"
+          backgroundColor="background.200"
+          _dark={{
+            backgroundColor: 'background.800',
+          }}
+        >
+          <Skeleton variant="quiz" height={10} width="full" />
+          <Skeleton variant="quiz" height={10} width="full" />
+          <Skeleton variant="quiz" height={10} width="full" />
+          <Skeleton variant="quiz" height={10} width="full" />
+        </Grid>
+      </VStack>
+    </Center>
   );
 };
